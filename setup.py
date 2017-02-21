3be66e4607bf54282848d22831c263320ef2ac74
Alexey Lavrenuke <direvius@gmail.com>
2017-02-21 15:30:34 +0300

    remove numpy and pandas from deps

--- setup.py
+++ setup.py
@@ -2,15 +2,13 @@ from setuptools import setup, find_packages
 
 setup(
     name='target',
-    version='0.0.1',
+    version='0.0.2',
     description='a performance measurement stub',
     maintainer='Alexey Lavrenuke (load testing)',
     maintainer_email='direvius@yandex-team.ru',
     packages=find_packages(exclude=["tests", "tmp", "docs", "data"]),
     install_requires=[
         'tornado>=4.4.2',
-        'pandas>=0.18.0',
-        'numpy>=1.11.0',
     ],
     setup_requires=[
         'pytest-runner',
